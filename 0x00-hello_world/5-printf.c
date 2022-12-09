4b63ee359bf9c0294ab18874c6a8bc383632f50d
kaptumg <kaptumgladys68@gmail.com>
2022-12-09 03:21:07 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints exactly with proper grammar, but the outcome is a piece of art,
+ * main - print exactly with proper grammar, but the outcome is a piece of art
  *
  * Return: 0 always
  *
