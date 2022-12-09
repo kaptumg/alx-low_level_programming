2b3a18d5c827c9f109d4c932c3b80dd74c3d11b9
kaptumg <kaptumgladys68@gmail.com>
2022-12-09 03:05:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  print the phrase "Programming is like building 
+ * a multilingual puzzle"
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
