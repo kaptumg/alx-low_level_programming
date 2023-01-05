59f7a5e398901a56449ce4a057ccf1cfd961929c
kaptumg <kaptumgladys68@gmail.com>
2023-01-05 14:02:48 -0800

    recurssion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _strcat - concantenates two strings
+ * @c: the character to be printed
+ *
+ * Return: 0 on succes 1
+ * on error, -1 is returned and error is set appropriately
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1);
+}
