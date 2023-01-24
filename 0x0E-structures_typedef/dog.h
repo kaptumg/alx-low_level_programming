d5850ec48858b622034f5e253ecdcc2cd616ea3a
kaptumg <kaptumgladys68@gmail.com>
2023-01-24 00:20:04 -0800

    5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,12 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - describes a dog and its attributes
+ * @name: First member, char pointer
+ * @age: Second member, float
+ * @owner: Third memeber, char pointer
+ * Description: This struct has three elements and is a new type dog
+ */
 
 struct dog
 {
@@ -6,3 +14,14 @@ struct dog
 	float age;
 	char owner;
 };
+/**
+ * dog - Typdef for dog type
+ */
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
